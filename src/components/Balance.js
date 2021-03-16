fe0218f12a3e2bac72afc93d33cbad19ee5cce83
Mehdi El <mehdielallam07@gmail.com>
2021-03-16 22:44:16 +0100

    Showing balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 export default function Balance() {
+
+    const { transactions } = useContext(AppContext
+        );
+    const total = transactions.map(tr => tr.amount).reduce((a,b) => parseInt(a) + parseInt(b) , 0)
+    console.log(total)
     return (
         <>
-            <h4>Your Balance</h4>
-            <h1 id="balance">$0.00</h1>
+            <h4 style={{textAlign: 'center'}}>Your Balance</h4>
+            <h1 style={{textAlign: 'center'}} id="balance">${total}</h1>
         </>
     )
 }
