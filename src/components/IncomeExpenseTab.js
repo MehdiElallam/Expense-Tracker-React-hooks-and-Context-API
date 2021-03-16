cabf0db1a8082c002e7cc7d24b51b322269659bc
Mehdi El <mehdielallam07@gmail.com>
2021-03-16 22:44:49 +0100

    Adding the new transaction form

--- src/components/IncomeExpenseTab.js
+++ src/components/IncomeExpenseTab.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useContext} from 'react'
+import { AppContext } from '../context/AppContext';
 
 export default function IncomeExpenseTab() {
+
+    const { transactions } = useContext(AppContext);
+    const income = transactions.map(tr => tr.amount )
+                            .filter(amount => amount > 0)
+                            .reduce((a,b) => parseInt(a) + parseInt(b), 0);
+    const expense = transactions.map(tr => tr.amount )
+                            .filter(amount => amount < 0)
+                            .reduce((a,b) => parseInt(a) + parseInt(b), 0);
     return (
         <div className="inc-exp-container">
             <div>
             <h4>Income</h4>
-            <p id="money-plus" className="money plus">+$0.00</p>
+            <p id="money-plus" className="money plus">+${income}</p>
             </div>
             <div>
             <h4>Expense</h4>
-            <p id="money-minus" className="money minus">-$0.00</p>
+            <p id="money-minus" className="money minus">${Math.abs(expense)}</p>
             </div>
         </div>
     )
--- src/components/NewTransaction.js
+++ src/components/NewTransaction.js
@@ -1,27 +1,50 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from 'react'
+import { AppContext } from '../context/AppContext';
 
 export default function NewTransaction() {
     
-    const [text, setText] = useState('');
-    const [amount, setAmount] = useState(0);
+    const [ text, setText ] = useState('');
+    const [ amount, setAmount ] = useState(0);
+    const [ textError, setTextError ] = useState('');
+    const [ amountError, setAmountError ] = useState('');
+
+    const { addTransac, transactions } = useContext(AppContext)
+
+    const addTransaction = (e) => {
+        e.preventDefault();
+
+        if(text !== '' && amount !== 0){
+            const transaction = {
+                id : transactions.length + 1,
+                text,
+                amount
+            }
+            addTransac(transaction)
+        }else{
+            text === '' ? setTextError('The text field cannot be empty') : setTextError('')
+            amount === 0 ? setAmountError('The amount cannot be null') : setAmountError('')
+        }
+    }
 
 
     return (
         <div className="new-transaction">
             <h3>Add new transaction</h3>
-            <form id="form">
+            <form id="form" onSubmit={addTransaction}>
                 <div className="form-control">
                 <label htmlFor="text">Text</label>
                 <input type="text" value={text} onChange={(e) => { setText(e.target.value) }} id="text" placeholder="Enter text..." />
+                    { textError !== '' && (<small style={{color: 'red'}}>{textError}</small>) }
                 </div>
                 <div className="form-control">
                 <label htmlFor="amount"
                     >Amount <br />
-                    (negative - expense, positive - income)</label
-                >
+                    (negative for expense, positive for income)
+                </label>
                 <input type="number" value={amount} onChange={(e) => { setAmount(e.target.value) }} id="amount" placeholder="Enter amount..." />
+                { amountError !== '' && (<small style={{color: 'red'}}>{amountError}</small>) }
                 </div>
-                <button className="btn">Add transaction</button>
+                <button type='submit' className="btn">Add transaction</button>
             </form>  
         </div>
     )
